bbd0a277e3f56e490d7096bcfba0aa5588bbafbc
Fabulousman23 <andrew.makiev@gmail.com>
2022-08-24 20:43:07 -0400

    corect id

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -60,7 +60,7 @@ module.exports.getAuthURL = async () => {
   };
 };
 
-module.exports.getCalendarEvents = event => {
+module.exports.getCalendarEvents = async (event) => {
   const oAuth2Client = new google.auth.oAuth2(
     client_id,
     client_secret,
