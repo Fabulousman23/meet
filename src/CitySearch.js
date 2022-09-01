8a34b86729fdbb842ea10e05d3f65d1bb01fa87d
Fabulousman23 <andrew.makiev@gmail.com>
2022-09-01 15:13:34 -0400

    add locations

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
     state = {
         query: '',
+        suggestions: []
     }
+    handleInputChanged = (event) => {
+        const value = event.target.value;
+        this.setState({ query: value });
+    }
+
     render() {
         return (
             <div className="CitySearch">
@@ -11,8 +17,15 @@ class CitySearch extends Component {
                     type="text"
                     className="city"
                     value={this.state.query}
+                    onChange={this.handleInputChanged}
                 />
                 <ul className="suggestions">
+                    {this.state.suggestions.map((suggestion) => (
+                        <li key={suggestion}>{suggestion}</li>
+                    ))}
+                    <li key='all'>
+                        <b>See all cities</b>
+                    </li>
                 </ul>
             </div>
         );
