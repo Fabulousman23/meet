dc162906a89da2763e4dcfb0bccdb424bca7a2e5
Fabulousman23 <andrew.makiev@gmail.com>
2022-09-01 14:23:39 -0400

    add etag

---
+++ src/mock-data.js
@@ -0,0 +1,12 @@
+const mockData = [
+    {
+        "kind": "calendar#event",
+        "etag": "\"p3308h454qfgum0g\"",
+    },
+    {
+        "kind": "calendar#event",
+        "etag": "\"3181159875584000\"",
+    }
+];
+
+export { mockData };
