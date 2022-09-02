a5a124903e51d9d4612c9013032a000e0c6510a6
Fabulousman23 <andrew.makiev@gmail.com>
2022-09-02 14:07:23 -0400

    add test

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -40,14 +40,7 @@ describe('<CitySearch /> component', () => {
             expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
         }
     });
-    test("selecting a suggestion should change query state", () => {
-        CitySearchWrapper.setState({
-            query: 'Berlin'
-        });
-        const suggestions = CitySearchWrapper.state('suggestions');
-        CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
-        expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
-    });
+
     test('suggestion list match the query when changed', () => {
         CitySearchWrapper.setState({ query: '', suggestions: [] });
         CitySearchWrapper.find(".city").simulate("change", {
@@ -58,6 +51,30 @@ describe('<CitySearch /> component', () => {
             return location.toUpperCase().indexOf(query.toUpperCase()) > -1;
         });
         expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
+    });
+    test("selecting a suggestion should change query state", () => {
+        CitySearchWrapper.setState({
+            query: 'Berlin'
+        });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+        expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
+    });
+    test('selecting CitySearch input reveals the suggestions list', () => {
+        CitySearchWrapper.find('.city').simulate('focus');
+        expect(CitySearchWrapper.state('showSuggestions')).toBe(true);
+        expect(CitySearchWrapper.find('.suggestions').prop('style')).not.toEqual({
+            display: 'none'
+        });
+    });
+
+    test('selecting a suggestion should hide the suggestions list', () => {
+        CitySearchWrapper.setState({ query: 'Berlin', showSuggestions: undefined });
+        CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+        expect(CitySearchWrapper.state('showSuggestions')).toBe(false);
+        expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({
+            display: 'none'
+        });
     });
 
 });
