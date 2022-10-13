aa6ea3d81a65914877c9e85a554f7f4fa4c643a0
Fabulousman23 <andrew.makiev@gmail.com>
2022-10-13 11:31:06 -0400

    remove notify atatus

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as atatus from 'atatus-spa';
 
 atatus.config('02fe77c4ef1f440d98c39c8f92d7e400').install();
-atatus.notify(new Error('Test Atatus Setup'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
