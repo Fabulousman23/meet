016ccb62362d4f9d69d07353ae61f3a998a71946
Fabulousman23 <andrew.makiev@gmail.com>
2022-10-13 11:25:50 -0400

    add atatus

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as atatus from 'atatus-spa';
 
-atatus.config('d40b3bc0ebac406cb8cde0eb660e934f').install();
+atatus.config('02fe77c4ef1f440d98c39c8f92d7e400').install();
 atatus.notify(new Error('Test Atatus Setup'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
