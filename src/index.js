9c065384af36f5e54bf60c30222db3f15c61bb7f
Fabulousman23 <andrew.makiev@gmail.com>
2022-10-17 18:30:52 -0400

    add register worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as atatus from 'atatus-spa';
+import * as serviceWorker from './service-worker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('02fe77c4ef1f440d98c39c8f92d7e400').install();
 
@@ -12,3 +14,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+serviceWorker.register();
+serviceWorkerRegistration.register();
